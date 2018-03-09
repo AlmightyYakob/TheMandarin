bde3d642cadb2d8cd500274554bd1cc2a9ae21d9
Jacob Nesbitt <jjnesbitt2@gmail.com>
2018-03-08 19:55:00 -0600

    Python scripts updated

--- routineTweet.py
+++ routineTweet.py
@@ -4,10 +4,16 @@ import glob
 import os
 import subprocess
 
+SECONDS_TO_SLEEP = 60
+
 while(True):
     list_of_files = glob.glob('/home/jake/twitterBot/torch-rnn/cv/*.t7')
     latest_file = max(list_of_files, key=os.path.getctime)
     print latest_file
 
-    commandString = "./sample.sh " + latest_file
+    commandString = "./sample.sh " + latest_file + " > /home/jake/twitterBot/SAMPLE.txt"
     subprocess.call(commandString, shell=True)
+
+
+    print("Sleeping for " + str(SECONDS_TO_SLEEP) + " seconds.")
+    time.sleep(SECONDS_TO_SLEEP)
