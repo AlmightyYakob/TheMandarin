8cdf469651d57e23a5d80c3f47cd0c73da5d28eb
Jacob Nesbitt <nesbitjj@clarkson.edu>
2018-12-15 14:48:56 -0500

    init

--- preProcess.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-INPUT_FILE=$1
-
-if [ -z $INPUT_FILE ]; then
-    python torch-rnn/scripts/preprocess.py --input_txt output.txt --output_h5 preProcess/my_data.h5 --output_json preProcess/my_data.json
-else
-    python torch-rnn/scripts/preprocess.py --input_txt $INPUT_FILE --output_h5 preProcess/my_data.h5 --output_json preProcess/my_data.json
-fi
