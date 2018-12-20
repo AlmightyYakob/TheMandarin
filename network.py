be5b47cb7e2ad427ec039426cec6e99505a4856e
Yakob <jjnesbitt2@gmail.com>
2018-12-19 22:00:50 -0500

    working prediction

--- network.py
+++ network.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM, CuDNNLSTM
 from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.models import load_model
 from keras.utils import np_utils
 import numpy as np
 
@@ -16,25 +17,25 @@ int_to_char = dict((i, c) for i, c in enumerate(chars))
 n_chars = len(raw_text)
 n_vocab = len(chars)
 
+dataX = []
+dataY = []
+for i in range(n_chars - SEQ_LENGTH):
+	seq_in = raw_text[i:i + SEQ_LENGTH]
+	seq_out = raw_text[i + SEQ_LENGTH]
+	dataX.append([char_to_int[char] for char in seq_in])
+	dataY.append(char_to_int[seq_out])
+
+n_patterns = len(dataX)
+# print("Total Patterns: ", n_patterns)
+
+X = np.reshape(dataX, (n_patterns, SEQ_LENGTH, 1))
+X = X/float(n_vocab)
+
+y = np_utils.to_categorical(dataY)
+
+#####################################
+
 def main():
-	dataX = []
-	dataY = []
-	for i in range(n_chars - SEQ_LENGTH):
-		seq_in = raw_text[i:i + SEQ_LENGTH]
-		seq_out = raw_text[i + SEQ_LENGTH]
-		dataX.append([char_to_int[char] for char in seq_in])
-		dataY.append(char_to_int[seq_out])
-
-	n_patterns = len(dataX)
-	print("Total Patterns: ", n_patterns)
-
-	X = np.reshape(dataX, (n_patterns, SEQ_LENGTH, 1))
-	X = X/float(n_vocab)
-	
-	print(X.shape)
-	exit()
-	y = np_utils.to_categorical(dataY)
-
 	model = Sequential()
 	model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
 	model.add(Dropout(0.2))
@@ -46,8 +47,33 @@ def main():
 	checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 	callbacks_list = [checkpoint]
 
+	model.fit(X, y, epochs=50, batch_size=64, callbacks=callbacks_list)
 
-	model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
+
+def test(filepath='weights-improvement-02-2.8127.hdf5', LENGTH=500):
+	# model = get_model()
+	# model.load_weights(filepath)
+	model = load_model(filepath)
+
+	startIndex = np.random.randint(0, len(dataX) - 1)
+	pattern = dataX[startIndex]
+	output = ''.join([int_to_char[v] for v in pattern])
+
+	for i in range(LENGTH):
+		print("Round", i)
+		x = np.reshape(pattern, (1, len(pattern), 1))
+		x = x/float(n_vocab)
+
+		pred_vector = model.predict(x)
+		pred = np.argmax(pred_vector)
+		output += int_to_char[pred]
+
+		pattern.append(pred)
+		pattern = pattern[1:]
+
+	return output
+
+
 
 if __name__ == "__main__":
    main()
--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 from tensorflow.keras.models import load_model
 import numpy as np
-from network import SEQ_LENGTH, n_chars, n_vocab, chars, int_to_char
+from network import test
+import os
+import re
+import numpy as np
 
-def generate_text(model, length):
-    num = [np.random.randint(n_vocab)]
-    y_char = [int_to_char[num[-1]]]
-    X = np.zeros((SEQ_LENGTH, 1))
+def main():
+    baseFileString = 'weights-improvement-'
+    files = os.listdir('./')
+    fileRegex = re.compile(baseFileString + '(\d+-([\d\.]+))\.hdf5')
 
-    for i in range(length):
-        pred = model.predict([X])
-        X[i] = pred
-        y_char.append(int_to_char[pred])
-
-    return ('').join(y_char)
+    matches = [fileRegex.match(x) for x in files]
+    matches = [x for x in matches if x != None]
+    minLoss = matches[np.argmin([x.group(2) for x in matches])].group()
+    print(test(minLoss, 200))
 
 
-model = load_model('weights-improvement-02-2.8127.hdf5')
-generate_text(model, 500)
+if __name__ == "__main__":
+   main()
